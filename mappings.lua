735f9f355de20e6a3286bd9810348bd92392d017
sickelap <sickelap@pm.me>
2024-05-20 10:10:10 +0100

    add shortcut to config files

--- mappings.lua
+++ mappings.lua
@@ -5,8 +5,8 @@ M.dap = {
     ["<leader>db"] = {
       "<cmd> DapToggleBreakpoint <CR>",
       "Toggle Breakpoint",
-    }
-  }
+    },
+  },
 }
 
 M.crates = {
@@ -15,9 +15,24 @@ M.crates = {
       function()
         require("crates").upgrade_all_crates()
       end,
-      "Upgrade all crates"
-    }
-  }
+      "Upgrade all crates",
+    },
+  },
+}
+
+M.finder = {
+  n = {
+    ["<leader>fc"] = {
+      function()
+        require("telescope.builtin").find_files {
+          prompt_title = "Config Files",
+          cwd = "~/.config/nvim/lua/custom",
+          follow = true,
+        }
+      end,
+      "Find config files",
+    },
+  },
 }
 
 return M
